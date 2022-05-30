9c439bd0d2fefd152bb0eeb649c1e5bf9abcdbd5
blendT <blend.tahiri@outlook.com>
2022-05-30 12:28:08 +0200

    added Slider

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import React from "react";
 import Header from "./components/Header/Header";
+import Slider from "./components/Slider/Slider";
 
 function App() {
-  return <Header />;
+  return (
+    <div>
+      <Header />
+      <main>
+        <Slider />
+      </main>
+    </div>
+  );
 }
 
 export default App;
