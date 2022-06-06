070e2d4a67ed6de0f6b579e0d3b513adeaa66d45
blendT <blend.tahiri@outlook.com>
2022-06-06 18:54:22 +0200

    added validation

--- src/UI/Input.js
+++ src/UI/Input.js
@@ -1,15 +1,35 @@
 import classes from "./Input.module.css";
+import { useRef, useImperativeHandle } from "react";
+import React from "react";
 
-const Input = (props) => {
+const Input = React.forwardRef((props, ref) => {
+  const inputRef = useRef();
+
+  const activate = () => {
+    inputRef.current.focus();
+  };
+
+  useImperativeHandle(ref, () => {
+    return { focus: activate };
+  });
+
+
   return (
-    <input
-      className={classes.input}
-      type={props.type}
-      placeholder={props.placeholder}
-      value={props.value}
-      onChange={props.onChange}
-    />
+    <div
+      className={`${classes.control} ${
+        props.isValid === false ? classes.isNotValid : ""
+      }`}
+    >
+      <input
+        ref={inputRef}
+        type={props.type}
+        id={props.id}
+        value={props.value}
+        onChange={props.onChange}
+        onBlur={props.onBlur}
+      />
+    </div>
   );
-};
+});
 
 export default Input;
